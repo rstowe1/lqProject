adfe9d6940334a16d90f11f9c1137d3bf2699903
Ryan <ryanwstowe@gmail.com>
2019-07-17 09:57:34 -0400

    wip

--- src/components/card.js
+++ src/components/card.js
@@ -3,56 +3,6 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import axios from 'axios';
-// import "filepond/dist/filepond.min.css";
-// import {makeStyles} from '@material-ui/core/styles';
-// import Table from './table'
-// import Upload from './upload';
-// import {FilePond} from "react-filepond";
-
-
-// STYLES
-// const useStyles = makeStyles({
-//   card: {
-//     borderRadius: '20px',
-//     paddingLeft: '5px',
-//     padding: '20px',
-//     marginLeft: '24px',
-//     marginRight: '24px',
-//     marginBottom: '10px'
-//   },
-//   bullet: {
-//     display: 'inline-block',
-//     margin: '2px 2px 10px 5px',
-//     padding: '2px 5px 5px 0',
-//     transform: 'scale(0.8)',
-//   },
-//   title: {
-//     fontSize: 14,
-//   },
-//   pos: {
-//     marginBottom: 12,
-//   },
-// });
-//
-// export default function SimpleCard(props) {
-//   const classes = useStyles();
-//   return (
-//     <Card className={classes.card}>
-//       <CardContent>
-//         <Typography className={classes.title} variant="h1">
-//           <h1>{props.title}</h1>
-//         </Typography>
-//         <Typography variant="body2" component="p">
-//           <h5>Monthly Payments</h5>
-//         </Typography>
-//         <FilePond server='http://localhost:8000/upload'
-//                   process='post'/>
-//         {/*<Table/>*/}
-//       </CardContent>
-//     </Card>
-//   );
-// }
-
 
 // TUTORIAL
 // https://programmingwithmosh.com/javascript/react-file-upload-proper-server-side-nodejs-easy/
