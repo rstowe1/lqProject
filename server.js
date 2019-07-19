c9379c189f5235594682b7e3b083f5820a42382a
Ryan <ryanwstowe@gmail.com>
2019-07-19 12:23:14 -0400

    wip

--- server.js
+++ server.js
@@ -8,23 +8,23 @@ app.use(cors());
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-  cb(null, 'public/upload')
-},
-filename: function (req, file, cb) {
-  cb(null, file.originalname )
-}
+    cb(null, 'public/upload')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname )
+  }
 });
 
 var upload = multer({ storage: storage }).single('file');
 
 app.post('/upload',function(req, res) {
-     
+
   upload(req, res, function (err) {
-         if (err instanceof multer.MulterError) {
-             return res.status(500).json(err)
-         } else if (err) {
-             return res.status(500).json(err)
-         }
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err)
+    } else if (err) {
+      return res.status(500).json(err)
+    }
     return res.status(200).send(req.file)
 
   })
--- src/components/card.js
+++ src/components/card.js
@@ -29,7 +29,7 @@ class SimpleCard extends React.Component {
   onClickHandler = () => {
     const data = new FormData();
     data.append('file', this.state.selectedFile);
-    axios.post("https://54.161.134.14/upload", data, {
+    axios.post("http://localhost:8000/upload", data, {
        // receive two    parameter endpoint url ,form data
    }).then(res => { // then print response status
     console.log(res.statusText)
