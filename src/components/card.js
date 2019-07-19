ba3d2e80b2b65529d90ac27ac3f7662ec3fcf676
Ryan <ryanwstowe@gmail.com>
2019-07-19 13:10:00 -0400

    wip

--- src/components/card.js
+++ src/components/card.js
@@ -28,8 +28,8 @@ class SimpleCard extends React.Component {
 
   onClickHandler = () => {
     const data = new FormData();
-    const URL = "http://localhost:8000/upload";
-    // const URL = "http://192.168.0.125:3000/upload";
+    // const URL = "http://localhost:8000/upload";
+    const URL = "https://ryan-test.com/upload";
 
     data.append('file', this.state.selectedFile);
     axios.post(URL, data, {
