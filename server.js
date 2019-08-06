38bd4549e9f77a2f1ce282d268dc5a7fd90f3baf
Ryan <ryanwstowe@gmail.com>
2019-08-06 13:58:30 -0400

    wip

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(helmet());
 app.use(cors());
 
 app.use((req, res, next) =>{
-  res.setHeader('Access-Control-Allow-Origin', 'https://ryan-test.com')
+  res.setHeader('Access-Control-Allow-Origin', 'https://ryan-test.com');
   next()
 });
 
