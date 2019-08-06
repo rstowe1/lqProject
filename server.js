f236786961d4cf7fcfa361d96b8b30ff3c0a136e
Ryan <ryanwstowe@gmail.com>
2019-08-06 14:04:17 -0400

    wip

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(cors());
 
 app.use((req, res, next) =>{
   res.setHeader('Access-Control-Allow-Origin', 'https://ryan-test.com/upload');
+  res.setHeader('Access-Control-Request-Method', 'POST');
   next()
 });
 
